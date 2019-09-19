0f092d07560312de539defcb8f0826b1a80949b1
Dmitry Verteyko <verteyko1990@gmail.com>
2019-09-19 23:21:38 +0300

    feature section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 // JS
 import './js/'
 import 'jquery';
+import 'slick-carousel'
 
 // SCSS
 import './assets/scss/core.scss'
+import $ from "jquery";
+require('slick-carousel/slick/slick.scss');
+require('slick-carousel/slick/slick-theme.scss');
+
+
+$('.lp-feature-slider').slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+});
