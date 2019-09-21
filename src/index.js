bc9cf057a18bc89d312e00437ffaba4ae8e4a677
Dmitry Verteyko <verteyko1990@gmail.com>
2019-09-21 12:29:07 +0300

    blogs section

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import 'slick-carousel'
 import './assets/scss/core.scss'
 import $ from "jquery";
 require('slick-carousel/slick/slick.scss');
+require('font-awesome/css/font-awesome.css');
 require('slick-carousel/slick/slick-theme.scss');
 require('slick-carousel/slick/slick-theme.scss');
 require('jquery-quicksand/jquery.quicksand');
@@ -19,7 +20,29 @@ $('.lp-feature-slider').slick({
     speed: 500,
     slidesToShow: 3,
     centerMode: true,
-    centerPadding: '20px'
+    centerPadding: '20px',
+    responsive: [
+        {
+            breakpoint: 1025,
+            settings: {
+                slidesToShow: 3,
+                centerMode: true,
+                centerPadding: '20px',
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 769,
+            settings: {
+                slidesToShow: 2,
+                centerMode: true,
+                centerPadding: '20px',
+                infinite: true,
+                dots: true
+            }
+        },
+        ]
 });
 
 $('.lp-clients-slider').slick({
@@ -30,6 +53,14 @@ $('.lp-clients-slider').slick({
     slidesToShow: 6,
     centerMode: true,
     centerPadding: '40px',
+});
+
+$('.lp-slider-row').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
 });
 
 $("#content").quicksand($("#data > li"),
